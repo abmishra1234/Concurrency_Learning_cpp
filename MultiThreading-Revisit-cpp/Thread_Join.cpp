14cc3a576a0180f36a89711f7bc40a646862ed38
Abinash Kumar Mishra <abmishra1234@gmail.com>
2021-12-16 04:48:34 +0530

    Update on comment and part of code with additional cases...

--- MultiThreading-Revisit-cpp/Thread_Join.cpp
+++ MultiThreading-Revisit-cpp/Thread_Join.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 // shared variable for managing common update for output
 int sum = 0;
+int mul = 1;
 
 
 void thSumFunction(const int& num01, const int& num02)
@@ -13,7 +14,8 @@ void thSumFunction(const int& num01, const int& num02)
 	// 1. shared varible
 	// 2. Direct messaging the Main thread
 	// So this approach is just shared the approach for shared variable...
-	sum = (num01+num02);
+	sum = (num01 + num02);
+	mul = (num01 * num02);
 }
 
 
@@ -23,7 +25,18 @@ int main()
 	cout << "First Include Thread header\n";
 	int a = 5, b = 6;
 	thread th(thSumFunction, a, b);
+	/*
+		Join method is ataching your worker thread with main thread and halting main thread from here until
+		worker thread finish and comeback to main thread...
+
+		This is not useful in real thime thread scenario...
+		It is just for join understandiuing...
+	*/
+	
 	th.join();
+
 	cout << "So output value : sum = " << sum << "\n";
+	cout << "So output value : mul = " << mul << "\n";
+
 	cout << "exiting from main\n";
 }
