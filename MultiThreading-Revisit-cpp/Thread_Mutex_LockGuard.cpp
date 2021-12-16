fa81fe92cfb3478ae06670b25f60f4e1d7a5efd6
Abinash Kumar Mishra <abmishra1234@gmail.com>
2021-12-16 05:55:07 +0530

    Adding file for Mutex and LockGuard. Mutes and LockGuard are for handling the Race Condition in Threads.

---
+++ MultiThreading-Revisit-cpp/Thread_Mutex_LockGuard.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<thread>
+#include<mutex>
+#include<list>
+
+
+using namespace std;
+
+namespace MyLockGuard
+{
+	std::mutex mtx;
+	std::list<int> mylist;
+
+	void AddToList(int data)
+	{
+		std::lock_guard<std::mutex> lkg(mtx);
+		mylist.push_front(data);
+		cout << "Adding to list is successful from th_id = " << std::this_thread::get_id() << "\n";
+	}
+}
+
+using namespace MyLockGuard;
+
+int main()
+{
+	std::thread th1(AddToList, 4);
+	std::thread th2(AddToList, 5);
+
+	th1.join();
+	th2.join();
+
+	auto it = mylist.begin();
+	for (; it != mylist.end(); ++it)
+	{
+		cout << (*it) << " ";
+	}
+	
+	cout << "\nExiting from main... th_id = " << std::this_thread::get_id() << "\n";
+}
